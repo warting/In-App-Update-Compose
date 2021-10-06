0051c9116ee2d1accdd8986fc3be51ad3fae67de
warting <stefan@warting.se>
2021-10-06 11:23:11 +0200

    enable build cache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,7 +22,21 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+plugins {
+    id("com.gradle.enterprise") version "3.4.1"
+}
+
+buildCache {
+    local {
+        isEnabled = true
+        directory = File(rootDir, "build-cache")
+        removeUnusedEntriesAfterDays = 30
+    }
+    remote<HttpBuildCache> {
+        isEnabled = false
+    }
+}
 
 rootProject.name = "In-app Update Compose"
-include ':app'
-include ':inappupdatecompose'
+include(":app")
+include(":inappupdatecompose")
