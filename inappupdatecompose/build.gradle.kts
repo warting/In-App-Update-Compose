99eacaba158517669a895e9ffb54fb1709c94a27
Stefan Wärting <stefan@warting.se>
2024-01-18 15:39:00 +0100

    Disable NewerVersionAvailable check

--- inappupdatecompose/build.gradle.kts
+++ inappupdatecompose/build.gradle.kts
@@ -56,6 +56,7 @@ android {
         abortOnError = true
         disable.add("LintBaseline")
         disable.add("GradleDependency")
+        disable.add("NewerVersionAvailable")
         checkDependencies = true
         checkGeneratedSources = false
         sarifOutput = file("../lint-results-inappupdatecompose.sarif")
